b1f538c6368dc51aa6e769663e10691618e6ff4a
Michal Muszynski <hi@geodev.me>
2025-08-06 22:20:52 +0200

    init

---
+++ eleventy.config.js
@@ -0,0 +1,30 @@
+import Image, { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    sharpOptions: {
+      animated: true,
+    },
+  });
+
+  eleventyConfig.setServerOptions({
+    watch: ["_site/**/*.css", "_site/**/*.js"],
+  });
+
+  eleventyConfig.addNunjucksAsyncShortcode("svgIcon", async (filename) => {
+    const metadata = await Image(`./src/assets/${filename}`, {
+      formats: ["svg"],
+      dryRun: true,
+    });
+    return metadata.svg[0].buffer.toString();
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+}
